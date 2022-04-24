f784b982650269601edfe2595e00ee186c9f39c7
unknown <sokolovserega1982@gmail.com>
2022-04-24 17:23:43 +0300

    fix: remove all props & relocate logic from App into components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,18 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { nanoid } from 'nanoid';
+import { useSelector } from 'react-redux';
 import toast, { Toaster } from 'react-hot-toast';
 
 import Form from '../Form';
 import Contacts from '../Contacts';
 import Filter from '../Filter';
 
-import {
-  setContactsAction,
-  delContactAction,
-  getContactsArr,
-} from '../../redux/contactsSlice';
-import { filterAction, getFilterValue } from '../../redux/filterSlice';
+import { getContactsArr } from '../../redux/contactsSlice';
 
 import { Container, TitleMain, TitleSecond } from './App.styled';
 
 function App() {
-  const dispatch = useDispatch();
-
   const contacts = useSelector(getContactsArr);
 
-  const filter = useSelector(getFilterValue);
-
-  const changeFilter = evt => dispatch(filterAction(evt.currentTarget.value));
-
-  const deleteContact = id => dispatch(delContactAction(id));
-
-  // =====================================================
-
-  // for filter
-  const normalizedFilter = filter.toLowerCase();
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-
-  // add new contact
-  const formSubmitHandler = data => {
-    // checking name for matches
-    const normalizedName = data.name.toLowerCase();
-    const isFoundName = contacts.some(
-      contact => contact.name.toLowerCase() === normalizedName
-    );
-    // if already exist - show message
-    if (isFoundName) {
-      toast.error(`${data.name} is already in contacts!`);
-      return;
-    }
-    // if not found, add new contact
-    const newData = { id: nanoid(5), ...data };
-    dispatch(setContactsAction(newData));
-    toast.success('Successfully added!');
-  };
-
-  // contacts were updated!
   useEffect(() => {
     if (contacts.length === 0) {
       toast.error('Phonebook is empty!');
@@ -71,10 +30,10 @@ function App() {
         }}
       />
       <TitleMain>Phonebook</TitleMain>
-      <Form onSubmit={formSubmitHandler} />
+      <Form />
       <TitleSecond>Contacts</TitleSecond>
-      <Filter value={filter} onChange={changeFilter} />
-      <Contacts arr={filteredContacts} onDelContact={deleteContact} />
+      <Filter />
+      <Contacts />
     </Container>
   );
 }
